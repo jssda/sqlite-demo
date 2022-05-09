c2029fad5ce12719635f3be32d20b8bdc108d6f5
jssd <jssdjing@gmail.com>
2022-05-09 21:27:31 +0800

    feat(global): init

---
+++ src/main/java/pers/jssd/sqlitedemo/SqliteDemoApplication.java
@@ -0,0 +1,16 @@
+package pers.jssd.sqlitedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author jssd
+ */
+@SpringBootApplication
+public class SqliteDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqliteDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pers/jssd/sqlitedemo/SqliteDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pers.jssd.sqlitedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqliteDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
